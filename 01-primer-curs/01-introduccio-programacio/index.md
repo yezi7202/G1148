b229efcc94ccb709cecab1587ce7b49adfc9d8d0
yezi <yezi7202@gmail.com>
2026-09-14 14:01:27 +0200

    vault backup: 2026-09-14 14:01:27

---
+++ 01-primer-curs/01-introduccio-programacio/index.md
@@ -0,0 +1,6 @@
+[[pla docent]]
+[[0-presentacio.pdf]]
+[[0-planficacio-alumnat.pdf]]
+
+[[t1-conceptes.pdf]] 
+[[t1-primers-contactes.pdf]]
--- 01-primer-curs/02-algorismica/codi/exercici1.py
+++ 01-primer-curs/02-algorismica/codi/exercici1.py
@@ -1,15 +1,21 @@
+# funció que retorna multiplicació de tres nombres enters
 def mult(a:int, b:int, c:int) -> int:
 	return a * b * c
 
 print (mult(1, 2, 3))
 
+# funció que mostra per pantalla 5 vegades "hola" i retornar "molt de gust"
 def funcio2 (a:str) -> str:
 	print (a * 5)
 	return "molt de gust"
 
 print(funcio2("hola"))
 
+# funció que compta les "a" contingudes en una paraula
 def comptar(a:str) -> int:
 	return a.count("a")
 
 print(comptar("hola"))
+
+
+print("'manel'")
