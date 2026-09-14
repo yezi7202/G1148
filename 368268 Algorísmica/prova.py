143e915478b789898a4be5962c15401fda30d3a6
yezi <yezi7202@gmail.com>
2026-09-14 11:22:27 +0200

    vault backup: 2026-09-14 11:22:27

---
+++ 368268 Algorísmica/prova.py
@@ -0,0 +1,15 @@
+def mult(a:int, b:int, c:int) -> int:
+	return a * b * c
+
+print (mult(1, 2, 3))
+
+def funcio2 (a:str) -> str:
+	print (a * 5)
+	return "molt de gust"
+
+print(funcio2("hola"))
+
+def comptar(a:str) -> int:
+	return a.count("a")
+
+print(comptar("hola"))
