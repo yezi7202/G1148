e2193a0b5426747704781b958818b8d28f0909b1
yezi <yezi7202@gmail.com>
2026-09-17 20:47:21 +0200

    vault backup: 2026-09-17 20:47:21

--- 01-primer-curs/01-intro-programacio/01-conceptes/practiques/01-primer-programa/intro_prog/practica3/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "salutacio.h"
-
-// llamamos o usamos la funcion 
-int main(void){
-	saludar();
-	return 0;
-}
-
--- 01-primer-curs/01-intro-programacio/01-conceptes/practiques/01-primer-programa/intro_prog/practica3/salutacio.h
+++
@@ -1,3 +0,0 @@
-//archivo cabecera para introducir declaraciones de funciones, variables y otros datos
-//declarar quiere decir que algo existe, con su nombre y tipo de valor que recibe y devuelve
-void saludar(void);
